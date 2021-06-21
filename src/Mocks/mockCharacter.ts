7d529ec4dca2f64abddcf3b38dd1ea62877f3e3b
dinujubd <maildinuju@gmail.com>
2021-06-22 01:39:21 +0700

    mock enhanced

--- src/Mocks/mockCharacter.ts
+++ src/Mocks/mockCharacter.ts
@@ -1,3 +1,26 @@
 import { Character } from "../Models/characters";
 
-export default {id:1, url: '', name: 'mock name', origin: {name: 'mock origin'}, location: {name: 'mock location'}} as Character
+const mockCharacter: Character = {
+    id: 1,
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+        name: "Earth (C-137)",
+        url: "https://rickandmortyapi.com/api/location/1"
+    },
+    location: {
+        name: "Earth (Replacement Dimension)",
+        url: "https://rickandmortyapi.com/api/location/20"
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [],
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: new Date("2017-11-04T18:48:46.250Z")
+}
+
+export default mockCharacter;
+
+
