6b6fc73b356cf3f0c05a0eaf8b993f280056ed9f
dinujubd <maildinuju@gmail.com>
2021-06-21 17:52:03 +0700

    Acrylic background fixed

--- src/Components/AcrylicBackground/AcrylicBackgroud.tsx
+++ src/Components/AcrylicBackground/AcrylicBackgroud.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Box, createStyles, makeStyles, Theme } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { ApplicationState } from '../../store/store';
-interface Character {
-    id: number,
-    name: string,
-    image: string
-}
-
+import { CharacterChunk } from '../../store/characters';
 
 interface StateProps {
-    currentCharacter?: Character;
+    currentCharacter?: CharacterChunk;
 }
 
-const useStyles = (img:string) => makeStyles((theme: Theme) =>
+const useStyles = (img: string) => makeStyles((theme: Theme) =>
     createStyles({
         root: {
             left: 0,
@@ -37,22 +32,21 @@ const useStyles = (img:string) => makeStyles((theme: Theme) =>
 );
 
 const AcrylicBackgroud: React.FC<StateProps> = (props) => {
+    const logoUrl = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg";
 
-    const classes = useStyles(props.currentCharacter?.image ?? 'https://rickandmortyapi.com/api/character/avatar/1.jpeg')();
+    const classes = useStyles(props.currentCharacter?.image ?? '')();
 
     return (
-        <div>
-            <img className={classes.logo} src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg" />
-            <div className={classes.root}></div>
-        </div>
+        <Box>
+            <img className={classes.logo} src={logoUrl} />
+            <Box className={classes.root}></Box>
+        </Box>
     );
 
 }
 
-const mapStateToProps = (state:ApplicationState):StateProps => {
-    return {
-        currentCharacter: state.characters.currentCharacter
-    }
-}
+const mapStateToProps = (state: ApplicationState): StateProps => ({
+    currentCharacter: state.characters.currentCharacter
+})
 
 export default connect(mapStateToProps)(AcrylicBackgroud);
