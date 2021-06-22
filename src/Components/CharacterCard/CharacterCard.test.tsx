85c5f7eeee3bc7d588c22a16ee70f48a4a454b23
dinujubd <maildinuju@gmail.com>
2021-06-22 08:43:54 +0700

    unncessary tests removed

--- src/Components/CharacterCard/CharacterCard.test.tsx
+++ src/Components/CharacterCard/CharacterCard.test.tsx
@@ -1,11 +1,7 @@
 import renderer from 'react-test-renderer';
 import { CharacterCard } from './CharacterCard';
-import configureMockStore from "redux-mock-store";
 import mockCharacter from '../../Mocks/mockCharacter';
 import { withMockStore } from '../../Hooks/withMockStore';
-
-const mockStore = configureMockStore();
-const store = mockStore({});
 
 describe('<CharacterCard />', () => {
     it('should renders correctly', () => {
